f8061ffa369e7d02260f727aa56110361c21ee4b
VID-bit <vidhanchabra93@gmail.com>
2025-05-29 18:53:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default function App() {
   return (
     <div
       style={{
-        display: 'flex',
-        height: '100vh',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '2rem',
-        background: '#f7f7f7',
-        color: '#333',
-        fontFamily: 'sans-serif'
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+        backgroundColor: "#f7f7f7",
+        color: "#333",
+        fontFamily: "sans-serif",
       }}
     >
       Welcome to Aro Room Designer – You’re Live!
